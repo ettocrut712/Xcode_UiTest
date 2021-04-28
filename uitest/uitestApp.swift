d930fec7b625615d171cc405cd97f6a8672f7268
normand <normandturcotte712@gmail.com>
2021-04-28 16:12:39 -0400

    Initial Commit

---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Normand Turcotte on 2021-04-28.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
